bccd5f03d5bece35cb26317d38d98045da006886
Maxim Murphy <maxim.murphy8@gmail.com>
2025-05-04 19:00:30 -0400

    Added setting svg icon

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LocationSunTime from "./components/LocationSunTime";
 import { SunProvider } from "./context/SunContext";
 import TimeDisplay from "./components/TimeDisplay";
 import Sun from "./components/Sun";
+import Settings from "./components/Settings";
 
 export default function Home() {
   return (
@@ -15,8 +16,9 @@ export default function Home() {
         <div className="z-20 absolute inset-0 flex flex-col items-center justify-center">
           <Countdown />
         </div>
-        <div className="z-20 h-full w-full flex justify-start items-end p-12">
+        <div className="z-20 h-full w-full flex justify-between items-end p-12">
           <TimeDisplay />
+          <Settings />
         </div>
       </div>
     </SunProvider>
