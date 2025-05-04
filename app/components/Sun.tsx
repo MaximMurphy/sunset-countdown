33c7afcd7423d86b810332f6d9c54607ead552fb
Maxim Murphy <maxim.murphy8@gmail.com>
2025-05-04 16:14:50 -0400

    Added initial sun object

---
+++ app/components/Sun.tsx
@@ -0,0 +1,7 @@
+export default function Sun() {
+  return (
+    <div className="absolute inset-0 flex justify-center">
+      <div className="w-75 h-75 bg-amber-500 rounded-full"></div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Countdown from "./components/Countdown";
 import LocationSunsetTime from "./components/LocationSunsetTime";
 import { SunsetProvider } from "./context/SunsetContext";
 import TimeDisplay from "./components/TimeDisplay";
+import Sun from "./components/Sun";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="relative h-screen flex flex-col items-center justify-center">
         <LocationSunsetTime />
         <Sky />
+        <Sun />
         <div className="z-20 absolute inset-0 flex flex-col items-center justify-center">
           <Countdown />
         </div>
