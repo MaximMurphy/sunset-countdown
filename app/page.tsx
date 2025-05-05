d59aaf09753cea930160999a344aafd64ce2770e
Maxim Murphy <maxim.murphy8@gmail.com>
2025-05-05 12:31:51 -0400

    Added grain overlay

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,22 @@ export default function Home() {
   return (
     <SunProvider>
       <div className="relative h-screen flex flex-col items-center justify-center">
+        <svg
+          viewBox="0 0 450 450"
+          xmlns="http://www.w3.org/2000/svg"
+          className="z-50 absolute inset-0 opacity-50 mix-blend-multiply"
+        >
+          <filter id="noiseFilter">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="9.32"
+              numOctaves="1"
+              stitchTiles="stitch"
+            />
+          </filter>
+
+          <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+        </svg>
         <LocationSunTime />
         <Sky />
         <Sun />
