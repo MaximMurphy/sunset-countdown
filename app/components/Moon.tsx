e11b9985dc83803387de8fb0a610cd85662d00bb
Maxim Murphy <maxim.murphy8@gmail.com>
2025-07-15 22:23:04 -0400

    Updated moon color to slate

--- app/components/Moon.tsx
+++ app/components/Moon.tsx
@@ -13,9 +13,9 @@ export default function Moon() {
           opacity: moonOpacity / 100,
         }}
       >
-        <div className="w-60 md:w-80 h-60 md:h-80 bg-gray-300 rounded-full" />
-        <div className="absolute top-2 md:top-4 w-60 md:w-80 h-60 md:h-80 bg-gray-400 rounded-full opacity-70" />
-        <div className="absolute top-4 md:top-8 w-60 md:w-80 h-60 md:h-80 bg-gray-500 rounded-full opacity-50" />
+        <div className="w-60 md:w-80 h-60 md:h-80 bg-slate-300 rounded-full" />
+        <div className="absolute top-2 md:top-4 w-60 md:w-80 h-60 md:h-80 bg-slate-400 rounded-full opacity-70" />
+        <div className="absolute top-4 md:top-8 w-60 md:w-80 h-60 md:h-80 bg-slate-500 rounded-full opacity-50" />
       </div>
     </div>
   );
